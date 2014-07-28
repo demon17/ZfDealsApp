d574f462721526fa8d0fba0f07da08cdd5c94456
FWP <avalos@mail.ru>
2014-07-28 17:39:46 +0300

    [Update] add new public with bootstrap, test controller

--- module/ZfDeals/config/module.config.php
+++ module/ZfDeals/config/module.config.php
@@ -30,8 +30,13 @@ return array(
 
     // admin Controller
     'controllers' => array(
+        /*
         'invokables' => array(
             'ZfDeals\Controller\Admin' => 'ZfDeals\Controller\AdminController'
+        ),
+        */
+        'factories' => array(
+            'ZfDeals\Controller\Admin' => 'ZfDeals\Controller\AdminControllerFactory'
         ),
     ),
 
@@ -42,6 +47,29 @@ return array(
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
+        ),
+    ),
+    // db connection part 1/2 next on config/autoload/local.php
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($sm) {
+                $config = $sm->get('Config');
+                $dbParams = $config['dbParams'];
+
+                return new Zend\Db\Adapter\Adapter(array(
+                    'driver'    => 'pdo',
+                    'dsn'       => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
+                    'database'  => $dbParams['database'],
+                    'username'  => $dbParams['username'],
+                    'password'  => $dbParams['password'],
+                    'hostname'  => $dbParams['hostname'],
+                ));
+            },
+            'ZfDeals\Mapper\Product' => function ($sm) {
+                return new \ZfDeals\Mapper\Product(
+                    $sm->get('Zend\Db\Adapter\Adapter')
+                );
+            },
         ),
     ),
 );
--- module/ZfDeals/src/ZfDeals/Controller/AdminController.php
+++ module/ZfDeals/src/ZfDeals/Controller/AdminController.php
@@ -3,24 +3,40 @@ namespace ZfDeals\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Form\Annotation\AnnotationBuilder;
 
 class AdminController extends AbstractActionController
 {
+    private $productAddForm;
+    private $productMapper;
+
     public function indexAction()
     {
         return new ViewModel();
     }
 
-    // add product controller
     public function addProductAction()
     {
-        $form = new \ZfDeals\Form\ProductAdd();
+        $form = $this->productAddForm;
 
         if ($this->getRequest()->isPost()) {
             $form->setData($this->getRequest()->getPost());
 
             if ($form->isValid()) {
-                // todo
+                $model = new ViewModel(
+                    array(
+                        'form' => $form
+                    )
+                );
+
+                try {
+                    $this->productMapper->insert($form->getData());
+                    $model->setVariable('success', true);
+                } catch (\Exception $e) {
+                    $model->setVariable('insertError', true);
+                }
+
+                return $model;
             } else {
                 return new ViewModel(
                     array(
@@ -35,5 +51,25 @@ class AdminController extends AbstractActionController
                 )
             );
         }
+    }
+
+    public function setProductAddForm($productAddForm)
+    {
+        $this->productAddForm = $productAddForm;
+    }
+
+    public function getProductAddForm()
+    {
+        return $this->productAddForm;
+    }
+
+    public function setProductMapper($productMapper)
+    {
+        $this->productMapper = $productMapper;
+    }
+
+    public function getProductMapper()
+    {
+        return $this->productMapper;
     }
 }
